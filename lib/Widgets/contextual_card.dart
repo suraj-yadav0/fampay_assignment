0dbb8dc04ca5f35843a03e3523762753c1398b26
Suraj Yadav <surajyadav200701@gmail.com>
2024-12-28 01:18:41 +0530

    minor refractrroing

--- lib/Widgets/contextual_card.dart
+++ lib/Widgets/contextual_card.dart
@@ -1,24 +1,5 @@
+
 import 'package:flutter/material.dart';
-
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        fontFamily: 'Inter',
-      ),
-      home: const HomeScreen(),
-    );
-  }
-}
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
